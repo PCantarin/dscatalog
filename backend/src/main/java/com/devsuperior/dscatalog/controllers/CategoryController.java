e0156633c9f4158a60b556ea8731256700e08070
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-05-13 08:54:44 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +20,7 @@ public class CategoryController {
 	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
